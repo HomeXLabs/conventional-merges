1938bde357ffbe48806888cb041c6c48797aa4dc
Jesse Wheeler <jesse@i-leet.com>
2020-01-10 10:52:56 -0500

    Updates to all

--- conventional.js
+++ conventional.js
@@ -1,39 +1,78 @@
-console.log('Conventional Merges 1.0')
-
-const mergeTitleField = document.getElementById('merge_title_field');
-const mergeButtons = document.querySelectorAll("button[data-disable-with='Merging…']");
+console.log('Conventional Merges 1.0');
 const titlePattern = /^(feat|chore|fix|ci|build|docs|style|refactor|perf|test)(\([a-z]+\)(!?):|(!?):) (.*)[^\.]$/;
 
-mergeTitleField.addEventListener('change', e => {
-  const mergeTitleValue = e.target.value;
-  if (!titlePattern.test(mergeTitleValue)) {
-    console.log('INVALID')
-    mergeButtons.forEach(mergeButton => {
-      mergeButton.setAttribute('disabled', 'disabled');
-      mergeButton.style.backgroundColor = '#d39494';
-    })
-  } else {
-    console.log('VALID')
-    mergeButtons.forEach(mergeButton => {
-      mergeButton.removeAttribute('disabled');
-    })
-  }
-  console.log('change', mergeTitleValue)
+/**
+ * "run_at": "document_end" wasn't operating as expected, instead we run at
+ * initial page load and attach a 'DOMContentLoaded' event to the window.
+ *
+ * This ensures the existence of each of our required elements.
+ */
+window.addEventListener('DOMContentLoaded', () => {
+  console.log('loaded');
+
+  const mergeTitleField = document.getElementById('merge_title_field');
+  const mergeButtons = document.querySelectorAll(
+    "button[data-disable-with='Merging…']",
+  );
+
+  /**
+   * The 'change' event isn't triggered when the value is programatically updated
+   * after selected a new merge type.
+   *
+   * (i.e. 'Merge pull request', 'Squash and merge').
+   *
+   * To resolve this, we add 'click' event listeners to each of these buttons to
+   * check the title again.
+   */
+  const mergeTypeButtons = document.querySelectorAll(
+    "button[data-details-container='.js-merge-pr']",
+  );
+
+  mergeTypeButtons.forEach(mergeTypeButton => {
+    mergeTypeButton.addEventListener('click', () => {
+      const { value } = mergeTitleField;
+      console.log('click', value);
+      handleMergeTitleChange(value, mergeTitleField, mergeButtons);
+    });
+  });
+
+  // Initial page load check.
+  handleMergeTitleChange(mergeTitleField.value, null, mergeButtons);
+
+  /**
+   * Add the various even listeners to the input field ('change', and 'input')
+   */
+  mergeTitleField.addEventListener('change', e => {
+    const { value } = e.target;
+    handleMergeTitleChange(value, null, mergeButtons);
+    console.log('change', value);
+  });
+
+  mergeTitleField.addEventListener('input', e => {
+    const { value } = e.target;
+    handleMergeTitleChange(value, null, mergeButtons);
+    console.log('input', value);
+  });
 });
 
-mergeTitleField.addEventListener('input', e => {
-  const mergeTitleValue = e.target.value;
-  if (!titlePattern.test(mergeTitleValue)) {
-    console.log('INVALID')
+/**
+ * Updates the input field with styles and disables the 'Confirm merge' buttons
+ * if the RegEx test fails.
+ *
+ * @param {string} titleValue
+ * @param {HTMLElement} titleElement
+ * @param {HTMLElement[]} mergeButtons
+ */
+function handleMergeTitleChange(titleValue, titleElement, mergeButtons) {
+  if (!titlePattern.test(titleValue)) {
     mergeButtons.forEach(mergeButton => {
       mergeButton.setAttribute('disabled', 'disabled');
       mergeButton.style.backgroundColor = '#d39494';
-    })
-  } else {
-    console.log('VALID')
-    mergeButtons.forEach(mergeButton => {
-      mergeButton.removeAttribute('disabled');
-    })
+    });
+    return;
   }
-  console.log('input', mergeTitleValue)
-});
+
+  mergeButtons.forEach(mergeButton => {
+    mergeButton.removeAttribute('disabled');
+  });
+}
