6412a206d650d3d78bc555231586cef14c1adcad
Jesse Wheeler <jwheeler@homex.com>
2020-01-09 22:53:11 -0500

    fix manifest

--- popup.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', function() {
-    var checkPageButton = document.getElementById('clickIt');
-    checkPageButton.addEventListener('click', function() {
-  
-      chrome.tabs.getSelected(null, function(tab) {
-        alert("Hello..! It's my first chrome extension.");
-      });
-    }, false);
-  }, false);
